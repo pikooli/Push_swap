2b55c7502400cbce4eae25a57d4badb0c7269769
pikooli <panamepoul@gmail.com>
2020-05-08 00:51:53 +0200

    test algo3

--- DirPush_swap/ft_algo_3/ft_algo_3.c
+++ DirPush_swap/ft_algo_3/ft_algo_3.c
@@ -8,13 +8,14 @@ char *ft_algo_1_3(t_list *a, t_list *b, t_option *option)
 
     if ((rest = ft_only_a_3(a, b, option)))
         return (rest);
+    if ((rest = ft_only_b_3(a, b, option)))
+        return (rest); 
     if ((rest = ft_a_3(a, b, option)))
         return (rest);
-    if ((rest = ft_a_b_3(a, b, option)))
-        return (rest);
-
     if ((rest = ft_b_3(a, b, option)))
         return (rest); 
+    if ((rest = ft_a_b_3(a, b, option)))
+        return (rest);
 
     return (ft_ra(a, option));
     printf("salut\n");
@@ -35,7 +36,7 @@ char    *ft_only_a_3(t_list *a, t_list *b, t_option *option)
         if (a->begin->next && a->begin->val > a->begin->next->val)
             return (ft_sa(a, option));
 
-        if (a->begin->val > a->last->val)
+        if (a->begin->val > a->last->val) 
             return (ft_rra(a, option));         
 
         if (ft_bigger_in_list(a))
@@ -55,25 +56,18 @@ char    *ft_a_3(t_list *a, t_list *b, t_option *option)
     return (NULL);
 }
 
-char    *ft_a_b_3(t_list *a, t_list *b, t_option *option)
+char    *ft_only_b_3(t_list *a, t_list *b, t_option *option)
 {
-    (void)option;
-    if (a->begin && b->begin)
-    {
-        
-    }
-    return (NULL);
-}
-
-char    *ft_b_3(t_list *a, t_list *b, t_option *option)
-{
-    if (b->begin)
+    if (b->begin && !a->begin)
     {   
         if (ft_check_rev_list(b) && !a->begin)
             return (ft_pa(a, b, option));
 
         if (ft_check_rev_list(b) && ft_check_list(a) &&
          b->begin->val < a->begin->val)
+            return (ft_pa(a, b, option));
+
+        if (ft_bigger_in_list(b))
             return (ft_pa(a, b, option));
 
         if (b->begin->val < b->last->val)
@@ -87,6 +81,26 @@ char    *ft_b_3(t_list *a, t_list *b, t_option *option)
     }
     return (NULL);
 }
+
+char    *ft_b_3(t_list *a, t_list *b, t_option *option)
+{
+    (void)a;
+    (void)b;
+    (void)option;
+    return (NULL);
+}
+char    *ft_a_b_3(t_list *a, t_list *b, t_option *option)
+{
+    if (a->begin && b->begin)
+    {
+        if (a->begin->val > b->begin->val && ft_bigger_in_list(b) && !ft_check_list(a))
+            return (ft_pb(a, b, option));
+        if (a->begin->val > a->last->val) // could remplace with rrr
+            return (ft_rra(a, option));
+    }
+    return (NULL);
+}
+
 
 
 /*
--- DirPush_swap/include/ft_algo_3.h
+++ DirPush_swap/include/ft_algo_3.h
@@ -3,9 +3,10 @@
 
 # include "main.h"
 
-char    *ft_b_3(t_list *a, t_list *b, t_option *option);
 char    *ft_a_3(t_list *a, t_list *b, t_option *option);
-char    *ft_a_b_3(t_list *a, t_list *b, t_option *option);
 char    *ft_only_a_3(t_list *a, t_list *b, t_option *option);
+char    *ft_b_3(t_list *a, t_list *b, t_option *option);
+char    *ft_only_b_3(t_list *a, t_list *b, t_option *option);
+char    *ft_a_b_3(t_list *a, t_list *b, t_option *option);
 
 #endif
